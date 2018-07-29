d9f8cb39b7e61d25105cd0b2221b678370fbba76
chethan <chethankumarbn87@gmail.com>
2018-07-30 00:28:34 +0530

    initial

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+
+//middleware
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        titlePage: 'Home Page',
+        welcomeMessage: 'Wow, Welcome to our website!'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.send(help.html);
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        titlePage: 'About Page'
+    });
+    // res.send({
+    //     name: 'Chethan',
+    //     likes: [
+    //         'Biking',
+    //         'Learning new things'
+    //     ]
+    // });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request.'
+    });
+});
+
+app.listen(8000, () => {
+    console.log('Server is up on port 8000');
+});
